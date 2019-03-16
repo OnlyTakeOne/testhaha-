5271f8ebb1635488683bbc1a38f5e4daf24ac59e
itcast <1062321131@qq.com>
2019-03-16 19:06:54 +0800

    修改user类，提交到本地仓库

--- src/main/java/com/cast/pingan/User.java
+++ src/main/java/com/cast/pingan/User.java
@@ -19,4 +19,19 @@ public class User {
     public void setAge(Integer age) {
         this.age = age;
     }
+    public void Test01(){
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.Test01();
+        user.Test01();
+        user.Test01();
+        user.Test01();
+    }
 }
